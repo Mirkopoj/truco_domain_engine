230a36f5c82acd7efb2de752bfe334c0afe10035
Mirko <mirkopoj@hotmail.com>
2024-07-13 23:56:24 -0300

    agregados Jugadores.
    Cambio de nombre y scope.

---
+++ src/jugador.rs
@@ -0,0 +1,20 @@
+use crate::carta::Carta;
+
+#[derive(Debug)]
+pub struct Jugador {
+    nombre: String,
+    mano: [Option<Carta>; 3],
+}
+
+impl Jugador {
+    pub fn new(nombre: String) -> Self {
+        Self {
+            nombre,
+            mano: [None; 3],
+        }
+    }
+
+    pub fn dar_mano(&mut self, mano: [Carta; 3]) {
+        self.mano = mano.map(Some);
+    }
+}
---
+++ src/mazo.rs
@@ -0,0 +1,38 @@
+use enum_iterator::all;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+use crate::{
+    carta::{Carta, Numero, Palo},
+    jugador::Jugador,
+};
+
+pub struct Mazo {
+    cartas: Vec<Carta>,
+}
+
+impl Mazo {
+    pub fn new() -> Mazo {
+        Mazo {
+            cartas: all::<Palo>()
+                .flat_map(|pal| all::<Numero>().map(move |num| Carta::new(num, pal)))
+                .collect(),
+        }
+    }
+
+    pub fn mezclar(&mut self) {
+        let mut rng = thread_rng();
+        self.cartas.shuffle(&mut rng);
+    }
+
+    pub fn repartir(&self, jugadores: &mut [Jugador]) {
+        let numero_de_jugadores = jugadores.len();
+        for (numero_de_jugador, jugador) in jugadores.iter_mut().enumerate() {
+            jugador.dar_mano([
+                self.cartas[numero_de_jugador],
+                self.cartas[numero_de_jugador + numero_de_jugadores],
+                self.cartas[numero_de_jugador + numero_de_jugadores * 2],
+            ]);
+        }
+    }
+}
